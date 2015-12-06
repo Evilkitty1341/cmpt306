ad89c63acd93f6af206b1097d864145fcd0e18e6
ksb961 <ksbellanger@gmail.com>
2015-12-05 19:47:39 -0600

    Added Intro Cut Scene
    - added cut scene animation
    - added cut scene dialogue
    - added gate animation

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,12 +2,28 @@
 using System.Collections;
 
 public class CameraControl : MonoBehaviour {
+
+	/*
+	 * currently constantly walks towards the player, nothing else is implemented
+	 * */
 	public GameObject followFemale;
 	public GameObject followMale;
+
+	//Temp Character Sprites for walk in sprite scene
+	public GameObject tempFemale;
+	public GameObject tempMale;
+
 	// Use this for initialization
 	void Start () {
+
+
 		followFemale = GameObject.Find ("FemalePlayerPrefab");
 		followMale = GameObject.Find ("MalePlayerPrefab");
+
+		//getting sprites for walk in sprite scene
+		tempFemale = GameObject.Find ("TempFemale");
+		tempMale = GameObject.Find ("TempMale");
+
 	}
 	
 	// Update is called once per frame
@@ -16,10 +32,14 @@ public class CameraControl : MonoBehaviour {
 		// Make the camera follow the player
 		if (PlayerSelect.getFemale ()) {
 			Destroy (followMale);
+			Destroy (tempMale);
 			this.transform.position = followFemale.transform.position + new Vector3 (0, 0, -10);
+
 		} else {
 			Destroy (followFemale);
+			Destroy (tempFemale);
 			this.transform.position = followMale.transform.position + new Vector3 (0, 0, -10);
+
 		}
 	}
 }
